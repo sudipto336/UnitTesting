b09037c6987f1d30712623ead5810641e80e3341
Sudipto Saha <sahasudipto336@gmail.com>
2022-11-13 11:59:28 +0530

    Add project files.

---
+++ TestingApplication/BasicMathsTests.cs
@@ -0,0 +1,36 @@
+﻿using DemoConsoleApplication;
+
+namespace TestingApplication
+{
+    [TestClass]
+    public class BasicMathsTests
+    {
+        BasicMaths basicMaths = new BasicMaths(); 
+
+        [TestMethod]
+        public void Test_Add()
+        {
+            Assert.AreEqual<double>(TestData.AdditionResult, basicMaths.Add(TestData.Data1, TestData.Data2));
+        }
+
+        [TestMethod]
+        public void Test_Substract()
+        {
+            Assert.AreEqual<double>(TestData.SubtractionResult, basicMaths.Substract(TestData.Data1, TestData.Data2));
+        }
+
+        [TestMethod]
+        public void Test_Multiply()
+        {
+            Assert.AreEqual<double>(TestData.MultiplionResult, basicMaths.Multiply(TestData.Data1, TestData.Data2));
+        }
+
+        [TestMethod]
+        public void Test_Divide()
+        {
+            Assert.AreEqual<double>(TestData.DivisionResult, basicMaths.Divide(TestData.Data1, TestData.Data2));
+        }
+
+
+    }
+}
---
+++ TestingApplication/TestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApplication
+{
+    internal class TestData
+    {
+        private const double data1 = 20;
+        private const double data2 = 10;
+        private const double additionResult = 30;
+        private const double subtractionResult = 10;
+        private const double multiplionResult = 200;
+        private const double divisionResult = 2;
+
+        public static double Data1 => data1;
+        public static double Data2 => data2;
+        public static double AdditionResult => additionResult;
+        public static double SubtractionResult => subtractionResult;
+        public static double MultiplionResult => multiplionResult;
+        public static double DivisionResult => divisionResult;
+    }
+}
